f3b6be5e794a1e5dfec6cc69baf1d0cbf1a0536a
Administrator <administrator@ubuntu.ubuntu-domain>
2010-08-12 17:56:54 -0300

    tentando ajustar imagens

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
+
+
   map.resources :usuarios
 
   map.resources :pacientes
-
-  map.resources :mains
-
+  
+    map.resources :mains
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -44,6 +47,7 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
+  
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
